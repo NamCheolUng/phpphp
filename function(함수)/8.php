aa8057126b5da4757c10e6454b27446018e7386c
NamCheolUng <nam92116@naver.com>
2021-07-29 18:28:15 +0900

    Update 8.php

--- function(함수)/8.php
+++ function(함수)/8.php
@@ -5,9 +5,10 @@
 	<title></title>
 </head>
 <body>
-<h1>정적변수</h1>
+<h1>정적변수</h1>                                 //함수 안에서만 쓰이는 지역 범위의 변수이면서도 값은 전역 범위 변수처럼 유지하는 변수
+
 <?php
-function increment(){
+function increment(){   
 	static $count=0;
 	echo $count.'<br>';
 	$count++;
