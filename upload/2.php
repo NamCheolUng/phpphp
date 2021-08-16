0cbe0f76bcecd66fb4fd4024a731a92527c4793e
NamCheolUng <nam92116@naver.com>
2021-08-16 19:47:10 +0900

    Update 2.php

--- upload/2.php
+++ upload/2.php
@@ -6,19 +6,20 @@
 </head>
 <body>
 <?php
-	ini_set("display_errors", "1");
-$uploaddir = 'C:\Bitnami\wampstack-8.0.8-0\apache2\htdocs\upload\file\\'; # 임시디렉토리에서 이동시키려고하는 파일디렉토리로 이동
-$uploadfile = $uploaddir.basename($_FILES['userfile']['name']); # 임시디렉토리 어느 파일명으로 되야하는지 지정 , $_FILES[] 업로드된 파일의 정보를 담고잇는 변수
-echo "<pre>";
-if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) { #파일이동
-	echo "파일이 유효하고, 성공적으로 업로드 되었습니다.\n";
-}else{
-	print "파일 업로드 공굑의 가능성이 있습니다!\n";
-}
-echo '자세한 디버깅 정보입니다.:';
-print_r($_FILES);
-print "</pre>";
+// 	ini_set("display_errors", "1");
+// $uploaddir = 'C:\Bitnami\wampstack-8.0.8-0\apache2\htdocs\upload\file\\'; # 임시디렉토리에서 이동시키려고하는 파일디렉토리로 이동  \\끝에2개 해야됨.
+// $uploadfile = $uploaddir.basename($_FILES['userfile']['name']); # 임시디렉토리 어느 파일명으로 되야하는지 지정 , $_FILES[] 업로드된 파일의 정보를 담고잇는 변수 , ['name']= 파일의 원래이름
+// echo '<pre>';
+// if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) { #파일이동
+// 	echo "파일이 유효하고, 성공적으로 업로드 되었습니다.\n";
+// }else{
+// 	print "파일 업로드 공격의 가능성이 있습니다!\n";
+// }
+// echo '자세한 디버깅 정보입니다.:';
+// print_r($_FILES);
+// print '</pre>';
+move_uploaded_file($_FILES['test']['tmp_name'],'./abcd'.$_FILES['test']['name']);
 ?>
-<img src="file/<?$_FILES['userfile']['name']?>" />
+<!-- <img src="file/<?=$_FILES['userfile']['name']?>" /> -->
 </body>
 </html>
