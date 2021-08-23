37a8ec9ae7dd817f500ed6fa79570c83147931db
NamCheolUng <nam92116@naver.com>
2021-08-23 20:37:36 +0900

    Update check.php

--- board/check.php
+++ board/check.php
@@ -6,24 +6,26 @@
 </head>
 <body>
 <?php
-     $conn = mysqli_connect("localhost", "root", "111111", "abc_corp");
+// error_reporting(0);
+  // header("Content-Type: application/json");
+      $conn = mysqli_connect("localhost", "root", "111111", "abc_corp");
 	
-	$uid = $_GET['uid'];
+	   $uid = $_POST['uid'];
+
 	 $sql = "SELECT * FROM members WHERE uid = '$uid'";
-        $result = mysqli_query($conn,$sql);
+	        $result = mysqli_query($conn,$sql);
         $row = mysqli_fetch_array($result);
 	
-	if($row==0){ #값이 참인지 거짓인지 판별하는것을 의미합니다. #이미 잇는 아이디 잇을시 중복된아이디입니다. 없을시 사용가능한 아이디입니다.
-		echo $uid.'사용가능한 아이디입니다.';
+	if($row==0){             #값이 참인지 거짓인지 판별하는것을 의미합니다. #이미 잇는 아이디가 존재할 시 중복된아이디입니다. 없을 시 사용가능한 아이디입니다.
+	 echo $uid.'는 사용가능한 아이디입니다.';
+		
     }else{
-		echo $uid.'중복된 아이디입니다.';
-
-    }
-	mysqli_close($conn);
-
+		echo $uid.'는 중복된 아이디입니다.';
+		 }
+mysqli_close($conn);    
 ?>
-<button type="button" onclick="window.close()">닫기</button>
 </body>
 </html>
+
 
 
